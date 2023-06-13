717b5ce5fd14b83ad93c39e61b93c19440769381
Xuan Long <longkt1366@gmail.com>
2023-06-14 00:55:50 +0700

    upload

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
-import initWebroutes from "./route/web"
+import initWebroutes from "./route/web";
+import connectDB from "./config/connectDB";
 require('dotenv').config();
 
 let app = express();
@@ -9,10 +10,12 @@ let app = express();
 //config app
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngine(app);
 initWebroutes(app);
+
+connectDB();
 
 let port = process.env.PORT || 6969;
 //port == undefine => port = 6969
