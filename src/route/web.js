4d874029a9da095417a8a8ded3d72ef4d7d57858
Xuan Long <longkt1366@gmail.com>
2023-06-13 23:33:59 +0700

    first nodejs

---
+++ src/route/web.js
@@ -0,0 +1,16 @@
+import express from "express";
+import homeController from "../controller/homeController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get("/", homeController.getHomePage);
+
+    router.get("/hoidanlong",(req , res) => {
+        return res.send("hê lố");
+    });
+
+    return app.use("/" , router);
+}
+
+module.exports = initWebRoutes;
