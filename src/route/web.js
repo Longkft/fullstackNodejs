e65a40de28df0917d50218a84bd80ef7cbcbc9ac
kinhdoanh365 <110050696+Longkft@users.noreply.github.com>
2023-08-17 11:09:55 +0700

    CRUD-Creat and Read

--- src/route/web.js
+++ src/route/web.js
@@ -5,10 +5,13 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
-
-    router.get("/hoidanlong",(req , res) => {
+    
+    router.get("/about",(req , res) => {
         return res.send("hê lố");
     });
+    router.get("/crud", homeController.getCRUD);
+    router.post("/post-crud", homeController.postCRUD);
+    router.get("/all-user", homeController.getCRUDUs);
 
     return app.use("/" , router);
 }
