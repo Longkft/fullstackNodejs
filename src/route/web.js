fbca2a63997c8f2cd341ad82842b9a1e55afea44
kinhdoanh365 <110050696+Longkft@users.noreply.github.com>
2023-09-24 23:28:19 +0700

    login

--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,13 @@
 import express from "express";
 import homeController from "../controller/homeController";
+import userController from "../controller/userController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
-    
-    router.get("/about",(req , res) => {
+
+    router.get("/about", (req, res) => {
         return res.send("hê lố");
     });
     router.get("/crud", homeController.getCRUD);
@@ -16,7 +17,9 @@ let initWebRoutes = (app) => {
     router.post("/update-user/:id", homeController.updateCRUDUser);
     router.post("/delete-user/:id", homeController.deleteUser);
 
-    return app.use("/" , router);
+    router.get("/api/login", userController.handleLogin);
+
+    return app.use("/", router);
 }
 
 module.exports = initWebRoutes;
